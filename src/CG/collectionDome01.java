db5c1a4673656522fed0f8c5fd1ff32ec9533e73
cg <2698678494@qq.com>
2021-09-12 19:10:55 +0800

    迭代器Iterator

--- src/CG/collectionDome01.java
+++ src/CG/collectionDome01.java
@@ -1,4 +1,23 @@
 package CG;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+/*
+* interface Iterator{
+*   boolean hasnext()
+*   Object next()
+* }
+* */
 public class collectionDome01 {
+    public static void main(String[] args) {
+        Collection collection=new ArrayList();
+        collection.add(100);
+        collection.add("陈淦");
+        Iterator it=collection.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+
 }
